1a05e182da9c3226667174cecd966417dcaaa7ce
Gleb Fomin <gleb.fom28@gmail.com>
2025-02-17 20:10:40 +0500

    chore: added marker custom icon

--- src/features/map-road-builder/map-road-builder.tsx
+++ src/features/map-road-builder/map-road-builder.tsx
@@ -4,7 +4,9 @@ import { useMapRoad } from './hooks/use-map-road';
 import { GeojsonUploader } from './partials/geojson-uploader';
 import { useState } from 'react';
 import { IGeoJsonCollection } from './map-road-builder.domain';
+import { icon } from 'leaflet';
 import styles from './map-road-builder.module.css';
+import markerIconPng from '/map-marker.png';
 import 'leaflet/dist/leaflet.css';
 
 const UAE_COORDINATES: [number, number] = [25.276987, 55.296249];
@@ -12,21 +14,17 @@ const UAE_COORDINATES: [number, number] = [25.276987, 55.296249];
 export function MapRoadBuilder() {
     const [dataGeoJson, setDataGeoJson] = useState<IGeoJsonCollection | null>(null);
 
-    const {
-        markerPositions,
-        routeCoords,
-        // isLoadingGeoJson,
-        // errorGeoJson,
-        // reloadGeoJson,
-        handleMarkerClick,
-        handleMapClick
-    } = useMapRoad(dataGeoJson);
+    const { markerPositions, routeCoords, handleMarkerClick, handleMapClick } = useMapRoad(dataGeoJson);
+
+    const customIcon = icon({
+        iconUrl: markerIconPng,
+        iconSize: [32, 32],
+        iconAnchor: [16, 32]
+    });
 
     return (
         <div style={{ width: '100%', height: '100%' }}>
             <GeojsonUploader setGeoJsonData={setDataGeoJson} />
-            {/*{isLoadingGeoJson && !errorGeoJson && <LoadingOverlay />}*/}
-            {/*{errorGeoJson && <ErrorOverlay errorText={errorGeoJson} reload={reloadGeoJson} />}*/}
             <MapContainer className={styles.map_container} center={UAE_COORDINATES} zoom={13} scrollWheelZoom>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -40,6 +38,7 @@ export function MapRoadBuilder() {
                         eventHandlers={{
                             click: () => handleMarkerClick(i)
                         }}
+                        icon={customIcon}
                     />
                 ))}
                 {routeCoords.length > 1 && (
